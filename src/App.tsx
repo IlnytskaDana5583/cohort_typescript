906353d2d201523a4cb4348a6f1b10d3f73d49f0
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-28 17:28:38 +0200

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+
+import GlobalStyles from "./styles/GlobalStyles";
+
 // Lessons
 import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
-import Homework_06 from "./homeworks/Homework_01/Homework_01";
 import Lesson_07 from "./lessons/Lesson_7/Lesson_07";
+import Lesson_08 from "./lessons/Lesson_08/Lesson_08";
+import Lesson_09 from "./lessons/Lesson_09/Lesson_09";
 
+import Lesson_11 from "./lessons/Lesson_11/Lesson_11";
 
-import "./App.css";
+// Homeworks
+import Homework_06 from "./homeworks/Homework_01/Homework_01";
 
 function App() {
   return (
-    <div className="app">
+    <>
+      <GlobalStyles />
       {/* Lessons */}
       {/* <Lesson_06 /> */}
-      {/* <Homework_06 /> */}
-      <Lesson_07 />
-
-
-
-
-
-
-    </div>
+      {/* <Lesson_07 /> */}
+      {/* <Lesson_08 /> */}
+      <Lesson_09 />
+              {/* <Lesson_11 /> */}
+      {/* Homeworks */}
+      {/* <Homework_07 /> */}
+      {/* <Homework_08 /> */}
+      {/* <Homework_09 /> */}
+    </>
   );
 }
 
